428c2ed5d4d19fafef832a21e58030099fe7ab6c
Tom <1840354092@qq.com>
2022-02-12 14:21:53 +0800

    default

--- zy-blog-server/controller/sms/blog.js
+++ zy-blog-server/controller/sms/blog.js
@@ -77,49 +77,31 @@ exports.articleCreate = async (req, res, next) => {
 //修改文章
 exports.articleUpdate = async (req, res, next) => {
     try {
-        let parms = req.body
-        let sql = $systemSqlMap.articleOpt.update
-        let queryClassData = $systemSqlMap.articleClassOpt.list + ` WHERE id='${parms.classId}'`
-        conn.query(queryClassData, function (err, result) {
-            if (err) {
-                console.log("错误", err)
-                let data = {
-                    error: 1,
-                    errMsg: '修改错误',
-                    data: err
-                }
-                res.json(data)
-            }
-            if (result) {
-                let data = [
-                    result[0].id,
-                    result[0].className,
-                    result[0].classValue,
-                    parms.isPublish,
-                    parms.title,
-                    parms.summary, parms.commentsCount, parms.img,
-                    parms.content, parms.isTop, parms.isHot, parms.pubTime,
-                    parms.insertTime,
+        let params = req.body,
+            sql = $systemSqlMap.articleOpt.update,
+            queryClassData = $systemSqlMap.articleClassOpt.list + ` WHERE id='${params.classId}'`
+
+        //查询分类数据
+        comMethods.commonQuery(queryClassData).then(data => {
+            let resData = data || {}
+            if (data.error) {
+                res.json(resData)
+            } else {
+                let updateParams = [
+                    resData.records[0].id,
+                    resData.records[0].className,
+                    resData.records[0].classValue,
+                    params.isPublish,
+                    params.title,
+                    params.summary, params.commentsCount, params.img,
+                    params.content, params.isTop, params.isHot, params.pubTime,
+                    params.insertTime,
                     tools.getDate(),
-                    parms.id,
+                    params.id,
                 ]
-                conn.query(sql, data, function (err, result) {
-                    if (err) {
-                        console.log("错误", err)
-                        let data = {
-                            error: 1,
-                            errMsg: '修改错误',
-                            data: err
-                        }
-                        res.json(data)
-                    }
-                    if (result) {
-                        let data = {
-                            error: 0,
-                            msg: '更新成功!'
-                        }
-                        res.json(data) //以json的方式返回客户端
-                    }
+                comMethods.commonQuery(sql, updateParams).then(data => {
+                    let realRes = data || {}
+                    res.json(realRes)
                 })
             }
         })
@@ -131,26 +113,12 @@ exports.articleUpdate = async (req, res, next) => {
 //删除文章
 exports.articleDelete = async (req, res, next) => {
     try {
-        let parms = req.body
-        let sql = $systemSqlMap.articleOpt.delete
-        let data = [parms.id,]
-        conn.query(sql, data, function (err, result) {
-            if (err) {
-                console.log("错误", err)
-                let data = {
-                    error: 1,
-                    errMsg: '删除失败',
-                    data: err
-                }
-                res.json(data)
-            }
-            if (result) {
-                let data = {
-                    error: 0,
-                    msg: '删除成功!'
-                }
-                res.json(data) //以json的方式返回客户端
-            }
+        let params = req.body,
+            sql = $systemSqlMap.articleOpt.delete,
+            deleteParams = [params.id,]
+        comMethods.commonQuery(sql, deleteParams).then(data => {
+            let realRes = data || {}
+            res.json(realRes)
         })
     } catch (err) {
         next(err)
@@ -159,31 +127,15 @@ exports.articleDelete = async (req, res, next) => {
 //发布文章
 exports.articlePublish = async (req, res, next) => {
     try {
-        let parms = req.body
-        let sql = $systemSqlMap.articleOpt.publish
-        let data = [
-            1,
-            tools.getDate(),
-            parms.id,
-        ]
-        conn.query(sql, data, function (err, result) {
-            if (err) {
-                console.log("错误", err)
-                let data = {
-                    error: 1,
-                    errMsg: '发布失败',
-                    data: err
-                }
-                res.json(data)
-            }
-            if (result) {
-                let data = {
-                    error: 0,
-                    msg: '发布成功!'
-                }
-                res.json(data) //以json的方式返回客户端
-            }
+        let params = req.body,
+            sql = $systemSqlMap.articleOpt.publish,
+            publishParams = [1, tools.getDate(), params.id,]
+
+        comMethods.commonQuery(sql, publishParams).then(data => {
+            let realRes = data || {}
+            res.json(realRes)
         })
+
     } catch (err) {
         next(err)
     }
@@ -193,8 +145,8 @@ exports.articlePublish = async (req, res, next) => {
 * */
 exports.articleClassList = async (req, res, next) => {
     try {
-        let parms = req.body, sql = '', total = 0;
-        let queryTotal = $systemSqlMap.articleClassOpt.count
+        let parms = req.body, sql = '', total = 0,
+            queryTotal = $systemSqlMap.articleClassOpt.count
         //多条件查询
         if (parms.params.id && parms.params.className) {
             sql = $systemSqlMap.articleClassOpt.list + ` WHERE id='${parms.params.id}' AND className='${parms.params.className}' ORDER BY ${parms.orderBy} ${parms.orderType} LIMIT ${parms.size} OFFSET ${parms.size * (parms.current - 1)}`
@@ -235,8 +187,10 @@ exports.articleClassCreate = async (req, res, next) => {
         //查询是否有重复的分类数据
         comMethods.commonQuery(queryRepeatClass).then(data => {
             let resData = data || {}
-            if (data.error || resData.records.length > 0) {
+            if (resData.records.length > 0) {
                 resData.errMsg = '分类值或分类名已存在'
+                resData.error = 1
+                //返回错误信息
                 res.json(resData)
             } else {
                 comMethods.commonQuery(sql, createParams).then(data => {
@@ -252,33 +206,20 @@ exports.articleClassCreate = async (req, res, next) => {
 //修改
 exports.articleClassUpdate = async (req, res, next) => {
     try {
-        let parms = req.body
-        let sql = $systemSqlMap.articleClassOpt.update
-        let data = [
-            parms.className,
-            parms.classValue,
-            parms.insertTime,
-            tools.getDate(),
-            parms.id,
-        ]
-        conn.query(sql, data, function (err, result) {
-            if (err) {
-                console.log("错误", err)
-                let data = {
-                    error: 1,
-                    errMsg: '修改错误',
-                    data: err
-                }
-                res.json(data)
-            }
-            if (result) {
-                let data = {
-                    error: 0,
-                    msg: '更新成功!'
-                }
-                res.json(data) //以json的方式返回客户端
-            }
+        let params = req.body,
+            sql = $systemSqlMap.articleClassOpt.update,
+            updateParams = [
+                params.className,
+                params.classValue,
+                params.insertTime,
+                tools.getDate(),
+                params.id,
+            ]
+        comMethods.commonQuery(sql, updateParams).then(data => {
+            let realRes = data || {}
+            res.json(realRes)
         })
+
     } catch (err) {
         next(err)
     }
@@ -286,26 +227,12 @@ exports.articleClassUpdate = async (req, res, next) => {
 //删除
 exports.articleClassDelete = async (req, res, next) => {
     try {
-        let parms = req.body
-        let sql = $systemSqlMap.articleClassOpt.delete
-        let data = [parms.id,]
-        conn.query(sql, data, function (err, result) {
-            if (err) {
-                console.log("错误", err)
-                let data = {
-                    error: 1,
-                    errMsg: '删除失败',
-                    data: err
-                }
-                res.json(data)
-            }
-            if (result) {
-                let data = {
-                    error: 0,
-                    msg: '删除成功!'
-                }
-                res.json(data) //以json的方式返回客户端
-            }
+        let params = req.body,
+            sql = $systemSqlMap.articleClassOpt.delete,
+            deleteParams = [params.id,]
+        comMethods.commonQuery(sql, deleteParams).then(data => {
+            let realRes = data || {}
+            res.json(realRes)
         })
     } catch (err) {
         next(err)
