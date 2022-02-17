d7e5f158bb8f235578d6ad02b262ae340d31267f
zy <1840354092@qq.com>
2022-02-17 17:12:10 +0800

    init

--- zy-blog-server/db.js
+++ zy-blog-server/db.js
@@ -2,24 +2,24 @@
  * 连接数据库文件
  * */
 module.exports = {
-    // mysql: {
-    //     host: "114.117.164.181",
-    //     user: "zy_blog",
-    //     password: "bYpEiwXdytCdsC8W",
-    //     database: "zy_blog",
-    //     port: '3306', // mysql链接端口
-    //     connectTimeout: 5000, //连接超时
-    //     multipleStatements: false //是否允许一个query中包含多条sql语句
-    // }
     mysql: {
-        host: "localhost",
-        user: "root",
-        password: "zhouyi....568568",
-        // password: "root",
-        database: "zy-blog",
+        host: "114.117.164.181",
+        user: "zy_blog",
+        password: "bYpEiwXdytCdsC8W",
+        database: "zy_blog",
         port: '3306', // mysql链接端口
         connectTimeout: 5000, //连接超时
         multipleStatements: false //是否允许一个query中包含多条sql语句
     }
+    // mysql: {
+    //     host: "localhost",
+    //     user: "root",
+    //     password: "zhouyi....568568",
+    //     // password: "root",
+    //     database: "zy-blog",
+    //     port: '3306', // mysql链接端口
+    //     connectTimeout: 5000, //连接超时
+    //     multipleStatements: false //是否允许一个query中包含多条sql语句
+    // }
 
 }
